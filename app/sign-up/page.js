9d0cafb06411e2176390ed46822adcd118362224
Cris1037 <128873608+Cris1037@users.noreply.github.com>
2025-04-09 13:50:31 -0600

    frey trade sign in and sign up

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -1,7 +1,4 @@
-//Google Icons: https://www.iconfinder.com/icons/7123025/logo_google_g_icon
-"use client";
-
-export default function SignIn() {
+export default function SignUp() {
     return (
         <div className="bg-[#46708D] h-screen w-screen flex flex-col justify-center items-center">
             <header className="justify-center items-center flex flex-col mb-10 relative mt-30">
@@ -21,13 +18,18 @@ export default function SignIn() {
                     className="p-4 w-100 h-10 border-[#A57730] border-2 rounded-md mb-5 justify-center items-center text-center placeholder:text-center placeholder:text-[#C4BB96] " 
                     required
                 />
-                <button className="bg-[#A57730] text-black font-bold py-2 px-4 rounded-md" >SIGN IN</button>
+                <input 
+                    placeholder="SECURITY QUESTION"
+                    className="p-4 w-100 h-10 border-[#A57730] border-2 rounded-md mb-5 justify-center items-center text-center placeholder:text-center placeholder:text-[#C4BB96] "
+                    required
+                />
+                <input placeholder="SECURITY ANSWER"
+                    className="p-4 w-100 h-10 border-[#A57730] border-2 rounded-md mb-5 justify-center items-center text-center placeholder:text-center placeholder:text-[#C4BB96] "
+                    required
+                />
+                <button className="bg-[#A57730] text-black font-bold py-2 px-4 rounded-md" >SIGN UP</button>
                 <div className="h-0 w-100 border-[#C4BB96] border-1 rounded-full mt-5"></div>
                 <button className="bg-white text-black font-bold p-2 rounded-md mt-5 w-30 flex items-center justify-center">SIGN IN<img src="/assets/7123025_logo_google_g_icon.png" width="36" height="36" className="ml-2"/>{/*https://www.iconfinder.com/icons/7123025/logo_google_g_icon*/}</button>
-                <div className="h-0 w-100 border-[#C4BB96] border-1 rounded-full mt-5"></div>
-                <p className="text-[#C4BB96] mt-10">DON'T HAVE AN ACCOUNT?</p>
-                <button className="text-[#C4BB96] border-b-2">SIGN UP</button>
-
             </main>
         </div>
     );
